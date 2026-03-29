154064e4421bbaf2687ade94b63f4d8e4d069493
Ray <rblez@proton.me>
2026-03-29 14:55:19 -0400

    Actualizar page.tsx

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -49,7 +49,7 @@ export default function Me() {
               <div className="absolute inset-0 bg-gradient-to-br from-black/20 to-white/20 dark:from-white/20 dark:to-black/20 rounded-full blur-3xl" />
               <div className="relative w-40 h-40 sm:w-48 sm:h-48 rounded-full overflow-hidden border-4 border-black/5 dark:border-white/5">
                 <Image
-                  src="/ray.png"
+                  src="/isotipo.png"
                   alt="Ray - rblez"
                   width={192}
                   height={192}
