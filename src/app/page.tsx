8b42400946829f686308ef18bb6491e52907bfd1
Ray <rblez@proton.me>
2026-04-01 13:49:55 -0400

    go

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
               <div className="absolute inset-0 bg-gradient-to-br from-black/20 to-white/20 dark:from-white/20 dark:to-black/20 rounded-full blur-3xl" />
               <div className="relative w-40 h-40 sm:w-48 sm:h-48 rounded-full overflow-hidden border-4 border-black/5 dark:border-white/5">
                 <Image
-                  src="/obito.png"
+                  src="/ray.png"
                   alt="Ray"
                   width={192}
                   height={192}
